6538c0e77052945eecb1158a0e7ce30667f2da37
Sajjadalgburi <dfgsajjad@gmail.com>
2024-04-20 17:28:38 -0400

    Added custom authentication error to handle an auth errors in feature

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,10 +1,16 @@
 const jwt = require('jsonwebtoken');
+const { GraphQLError } = require('graphql');
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
@@ -28,12 +34,13 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
+    // return the request object so it can be passed to the resolver as `context`
+
     // send to next endpoint
     next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
