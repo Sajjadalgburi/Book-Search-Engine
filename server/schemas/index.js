2252c80fdaecc77abb3f7cc8727814cebd3a4b38
Sajjadalgburi <dfgsajjad@gmail.com>
2024-04-20 17:08:24 -0400

    Importing the typeDefs/resolvers  into a common file and exporting them

---
+++ server/schemas/index.js
@@ -0,0 +1,8 @@
+// Importing GraphQL type definitions
+const typeDefs = require('./typeDefs');
+
+// Importing GraphQL resolvers
+const resolvers = require('./resolvers');
+
+// Exporting type definitions and resolvers
+module.exports = { typeDefs, resolvers };
