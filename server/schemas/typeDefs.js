0d2869b64883b54d83cc189512475463caa7e178
Sajjadalgburi <dfgsajjad@gmail.com>
2024-04-20 17:05:59 -0400

    Added comments via CHATGPT for better understanding

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,27 +1,29 @@
 const gql = require('graphql-tag');
 
+// Defining the GraphQL schema using gql template literal
 const typeDefs = gql`
+  # Defining the 'Users' type with fields
   type Users {
     _id: ID
     username: String
     email: String
     password: String
-    savedBooks: [String]!
+    savedBooks: [String]! # Array of strings representing saved books
   }
 
-
+  # Defining the queries available in the schema
   type Query {
-    users: [Users]!
-    singleUser: (userId: ID!): Users
-    me: Users
+    users: [Users]! # Query to retrieve all users
+    singleUser(userId: ID!): Users # Query to retrieve a single user by ID
+    me: Users # Query to retrieve the authenticated user
   }
 
-  
+  # Defining the authentication response type
   type Auth {
-    token: ID!
-    singleUser: Users
+    token: ID! # Authentication token
+    singleUser: Users # The user associated with the authentication
   }
-
 `;
 
+// Exporting the GraphQL schema
 module.exports = typeDefs;
