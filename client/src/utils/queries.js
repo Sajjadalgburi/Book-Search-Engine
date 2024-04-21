96bc4a2b4ec684f91cc881c1c46af783d81b9e86
Sajjadalgburi <dfgsajjad@gmail.com>
2024-04-21 19:46:20 -0400

    Implementing the query to for single users

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -4,9 +4,21 @@ const gql = require('@apollo/client');
 // Define the GraphQL query using the gql tag
 export const QUERY_USERS = gql`
   query allUsers {
-    // Fetch the _id, username, and savedBooks fields for all users
+    # Fetch the _id, username, and savedBooks fields for all users
     _id
     username
     savedBooks
+  }
+`;
+
+// GraphQL query to fetch a single user by their userId
+export const QUERY_SINGLE_USER = gql`
+  query singleUser($userId: ID!) {
+    # Fetching a single user by their userId
+    singleUser(userId: $userId) {
+      _id
+      username
+      savedBooks
+    }
   }
 `;
