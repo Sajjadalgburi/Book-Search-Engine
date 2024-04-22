60900adc3807f16d5df76184388015da739c9ff0
Sajjadalgburi <dfgsajjad@gmail.com>
2024-04-21 20:29:10 -0400

    Implemented the query to find the user saved in context/logged in

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,5 @@
 // Import the gql tag from the '@apollo/client' package
-const gql = require('@apollo/client');
+import { gql } from '@apollo/client';
 
 // Define the GraphQL query using the gql tag
 export const QUERY_USERS = gql`
@@ -16,6 +16,19 @@ export const QUERY_SINGLE_USER = gql`
   query singleUser($userId: ID!) {
     # Fetching a single user by their userId
     singleUser(userId: $userId) {
+      _id
+      username
+      savedBooks
+    }
+  }
+`;
+
+// Define a GraphQL query constant QUERY_ME
+export const QUERY_ME = gql`
+  // Define a GraphQL query named "me"
+  query me {
+    # Request fields for the currently authenticated user
+    me {
       _id
       username
       savedBooks
