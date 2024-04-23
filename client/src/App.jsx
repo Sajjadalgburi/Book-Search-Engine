a29586763f16cb421051dfc57a390306ef23c072
Sajjadalgburi <dfgsajjad@gmail.com>
2024-04-23 12:16:48 -0400

    wrapped the components in apolloprovider so that every request can work with apollo server (backend)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css';
-import { Outlet } from 'react-router-dom';
-
+import { Outlet } from 'react-router-dom'; // Import Outlet for routing
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'; // Import Apollo Client dependencies
 import Navbar from './components/Navbar';
+
+// Create an instance of Apollo Client with URI and cache
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <div>
+        <Navbar />
+        <Outlet />
+      </div>
+    </ApolloProvider>
   );
 }
 
-export default App;
+export default App; // Export the App component
